69845ae4454baf35f3c7249692a8a17aaa810eaf
Veronika Shvarts <yagodka-veronika@mail.ru>
2023-03-15 22:53:44 +0500

    added HW6

---
+++ task_HW41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int Entry(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] ArrFill(int lenght)
+{
+    int[] arr = new int[lenght];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Entry($"Введите число в эелемент массива под индексом {i}");
+    }
+    return arr;
+}
+
+string ArrMinusPlus(int[] array, bool plus = true)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(plus) 
+        {
+            if(array[i] > 0) count++;
+        }
+        else if(array[i] < 0) count++;
+    }
+    return plus ? $"положительных чисел {count}" : $"отрицательных чисел {count}";
+}
+
+
+int lenght = Entry("Введите длину массива");
+int[] arr = ArrFill(lenght);
+lenght = Entry("Введите\n1 чтобы показать количество положительных чисел"+
+               "\n0 чтобы показать количество отрицательных чисел");
+bool plus = lenght > 0? true: false;
+Console.Write($"[{string.Join(", ", arr)}] -> {ArrMinusPlus(arr, plus)}");
---
+++ task_HW43/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения 
+// b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double Entry(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double
+b1 = Entry("Введите значение b1"),
+k1 = Entry("Введите значение k1"),
+b2 = Entry("Введите значение b2"),
+k2 = Entry("Введите значение k2"),
+x = 0,
+y = 0;
+
+if (k1 == k2 && b1 == b2) Console.WriteLine("Прямые совпадают");
+else if (k1 == k2) Console.WriteLine("Прямые паррарельны");
+else
+
+{
+
+    x = (b2 - b1) / (k1 - k2);
+
+    y = k1 * x + b1;
+}
+
+Console.WriteLine($"точка пересечения двух прямых X {x}; Y {y}");
